0d416b82ede93fbecea3c712e11a9025192740f2
Tianen <teann.chen@gmail.com>
2019-02-14 20:20:33 -0600

    all files

---
+++ knnclassfier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#knnclassifier.py by Tianen Chen
+#netid: tchen335
+#2/10/2019
+
+import math
+import numpy
+import pandas
+import json
+import sys
+
+def main():
+	for i in range(2, len(sys.argv) - 1):
+		print(sys.argv[i])
+
+
+
+if __name__ == '__main__':		
+	main()
