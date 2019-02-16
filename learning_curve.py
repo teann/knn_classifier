e805a3c2df09909d9305dce6db8d6d7f572783d6
Tianen <teann.chen@gmail.com>
2019-02-16 16:45:33 -0600

    first commit of learning curve

---
+++ learning_curve.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python3.6 learning_curve.py
+
+import math
+import numpy as np
+import json
+import pandas as pd
+import sys
+from pprint import pprint
+import collections
+from operator import itemgetter
+
+def main():
+	maxk = int(sys.argv[1])
+	train_file = open(sys.argv[2])
+	val_file = open(sys.argv[3])
+	test_file = open(sys.argv[4])
+
+	train_set = json.load(train_file)
+	test_set = json.load(test_file)
+	val_set = json.load(val_file)
+
+	sys.stdout = open("out.txt" , "w")
+
+	train_df = pd.DataFrame(train_set['data'])
+	test_df = pd.DataFrame(test_set['data'])
+	val_df = pd.DataFrame(val_set['data'])
+
+	metadata_df = pd.DataFrame(train_set['metadata'])
+	labels = metadata_df['features']
+	labels1 = labels[len(labels) - 1]
+	#print(labels1[1])
+	dictOfLabels = collections.OrderedDict()
+	for la in labels1[1]:
+		dictOfLabels[la] = 0
+
+	train_mean = train_df.loc[:,train_df.columns != len(train_df.columns)-1].select_dtypes(include=[np.number]).mean()
+	train_std =  train_df.loc[:,train_df.columns != len(train_df.columns)-1].select_dtypes(include=[np.number]).std(ddof = 0)
+	train_std = train_std.replace(0, 1)
+
+	num_train = train_df.select_dtypes(include=[np.number])
+	num_test = val_df.select_dtypes(include=[np.number])
+
+	#print(test_df)
+	words_train = train_df.select_dtypes(exclude=[np.number])
+	words_test = val_df.select_dtypes(exclude=[np.number])
+
+	validator = np.array(val_df.iloc[:,-1])
+	#filler, gotta change this later
+	
+	dist_dict = collections.OrderedDict()
+
+	if (len(num_test.columns) > 0):
+		stan_train = getStan(train_mean, train_std, num_train)
+		stan_test = getStan(train_mean, train_std, num_test)
+
+		np_tr = np.array(stan_train)
+		np_test = np.array(stan_test)
+
+		dist_dict = computeManhattan(np_tr, np_test)
+	#	print(dist_dict)
+		(stringtoprint, minimumk) = getMin(maxk, dist_dict, dictOfLabels, validator, 0)
+	#	print(dist_dict)
+		####RETRAIN
+
+
+	if (len(words_test.columns) > 0):
+		word_tr = np.array(words_train)
+		word_test = np.array(words_test)
+
+		ham_dict = computeHamming(word_tr, word_test, dist_dict)
+		(stringtoprint, minimumk) = getMin(maxk, ham_dict, dictOfLabels, validator, 0)
+		####RETRAIN
+	print(*stringtoprint, sep ='\n')
+	print(minimumk)
+########################################################SPLITS HERE
+
+	trainval_df =  train_df.append(val_df)
+	train_mean = trainval_df.loc[:,trainval_df.columns != len(trainval_df.columns)-1].select_dtypes(include=[np.number]).mean()
+	train_std =  trainval_df.loc[:,trainval_df.columns != len(trainval_df.columns)-1].select_dtypes(include=[np.number]).std(ddof = 0)
+	train_std = train_std.replace(0, 1)
+
+
+	num_train = trainval_df.select_dtypes(include=[np.number])
+	num_test = test_df.select_dtypes(include=[np.number])
+
+	#print(test_df)
+	words_train = trainval_df.select_dtypes(exclude=[np.number])
+	words_test = test_df.select_dtypes(exclude=[np.number])
+
+	validator = np.array(test_df.iloc[:,-1])
+	#filler, gotta change this later
+	
+	dist_dict = collections.OrderedDict()
+
+
+	if (len(num_test.columns) > 0):
+		stan_train = getStan(train_mean, train_std, num_train)
+		stan_test = getStan(train_mean, train_std, num_test)
+
+		np_tr = np.array(stan_train)
+		np_test = np.array(stan_test)
+
+		dist_dict = computeManhattan(np_tr, np_test)
+	#	print(dist_dict)
+		stringtoprint = getMin(minimumk, dist_dict, dictOfLabels, validator, 1)
+	#	print(dist_dict)
+		####RETRAIN
+
+
+	if (len(words_test.columns) > 0):
+		word_tr = np.array(words_train)
+		word_test = np.array(words_test)
+
+		ham_dict = computeHamming(word_tr, word_test, dist_dict)
+		stringtoprint = getMin(minimumk, ham_dict, dictOfLabels, validator, 1)
+
+	print(*stringtoprint)
+
+
+def nansumwrapper(a, **kwargs):
+    if np.isnan(a).all():
+        return np.nan
+    else:
+        return np.nansum(a, **kwargs)
+
+
+def getMin(maxk, distance_dict, labels, validator, final):
+	listOfK = []
+	stringarray = []
+	if final == 1:
+		k = maxk
+		total = 0
+		correct = 0
+		for i in distance_dict:
+		#	print(i)
+			total += 1
+			sortedguy = sorted(distance_dict[i], key=lambda tup:tup[1])
+			firstk = sortedguy[0:k]
+			d = labels.fromkeys(labels, 0)
+			for tuples in firstk:
+				if tuples[2] in d:
+					d[tuples[2]] += 1
+			maximum = max(d, key=d.get)
+			if maximum == validator[i - 1]:
+				correct += 1
+		percentageCorrect = correct/total
+		stringarray.append(percentageCorrect)
+		return stringarray
+	else:
+		for k in range(1,maxk + 1):
+			total = 0
+			correct = 0
+			for i in distance_dict:
+			#	print(i)
+				total += 1
+				sortedguy = sorted(distance_dict[i], key=lambda tup:tup[1])
+				firstk = sortedguy[0:k]
+				d = labels.fromkeys(labels, 0)
+				for tuples in firstk:
+					if tuples[2] in d:
+						d[tuples[2]] += 1
+				maximum = max(d, key=d.get)
+				if maximum == validator[i - 1]:
+					correct += 1
+			percentageCorrect = correct/total
+			listOfK.append([k, percentageCorrect])
+			stringarray.append(str(k) + ',' + str(percentageCorrect))
+
+		sortedlist = sorted(listOfK, key=itemgetter(1), reverse=True)
+			#print(total)
+		return (stringarray, sortedlist[0][0])
+			
+
+
+
+def getStan(dfmean, dfstd, df):
+	stan_df = (df- dfmean)/dfstd
+	#stan_df[len(stan_df.columns)] = df.iloc[:,-1]
+	stan_df[len(stan_df.columns) - 1] = df.iloc[:,-1]
+	return stan_df
+
+
+def computeManhattan(stan_train, stan_test):
+	dict_dist = collections.OrderedDict()
+	instance1 = 0
+	for row in stan_test:
+		instance1 += 1
+		list_of_instances = []
+		dict_dist[instance1] = list_of_instances
+		instance = 0
+		rowtosub = row[0:len(row) - 1]
+		for row2 in stan_train:
+			instance += 1
+			label = row2[-1]
+			iterrow = row2[0:len(row2) - 1]
+			newone = np.absolute(np.subtract(rowtosub, iterrow))
+			distance = np.sum(newone)
+			dict_dist[instance1].append((instance, distance, label))	
+	#print(instance1)
+	return dict_dist
+
+def computeHamming(words_train, words_test, ham_dict):
+	ham_dict = collections.OrderedDict()
+	instance1 = 0
+	for row in words_test:
+	#	print(row)
+		instance1 += 1
+		list_of_instances = []
+		ham_dict[instance1] = list_of_instances
+		instance = 0
+		rowtosub = row[:len(row) - 1]
+		for row2 in words_train:
+			instance += 1
+			label = row2[-1]
+			iterrow = row2[:len(row2) - 1]
+			newArray = np.equal(iterrow, rowtosub)
+			distance = np.size(newArray) - np.count_nonzero(newArray)
+			instance_append = ((instance, distance, label))
+			ham_dict[instance1].append(instance_append)
+
+	return ham_dict
+
+if __name__ == '__main__':
+	main() 
